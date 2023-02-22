cfd1be83b0391371238ca74df731408457748fc5
Jonathan Talcott <jetalcott@milcorp.com>
2023-02-22 12:51:25 -0500

    Slight optimization to IConnectionConverter Due to having no static registry field

--- Connection/IConnectionConverter.cs
+++ Connection/IConnectionConverter.cs
@@ -18,7 +18,7 @@ namespace MILAV.API.Connection
             if (token is JObject jObject)
             {
                 var ttype = AppDomain.CurrentDomain.GetAssemblies().SelectMany(assembly => assembly.GetTypes())
-                    .First(type => typeof(IConnection).IsAssignableFrom(type) && Attribute.GetCustomAttribute(type, typeof(JsonObjectAttribute)) != null && ((JsonObjectAttribute)Attribute.GetCustomAttribute(type, typeof(JsonObjectAttribute))).Id == (string)jObject["type"]);
+                    .First(type => ((JsonObjectAttribute)Attribute.GetCustomAttribute(type, typeof(JsonObjectAttribute)))?.Id == (string)jObject["type"]);
                 return JsonConvert.DeserializeObject(token.ToString(Formatting.None), ttype);
             }
 
