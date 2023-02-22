37413ac3f758a8b96bb399cfd072cc8282bc6ef1
Jonathan Talcott <jetalcott@milcorp.com>
2023-02-22 13:10:33 -0500

    Added comments to the 2 JSON converter classes

--- Connection/IConnectionConverter.cs
+++ Connection/IConnectionConverter.cs
@@ -9,6 +9,7 @@ namespace MILAV.API.Connection
     {
         public override bool CanConvert(Type objectType)
         {
+            // True for types that are castable to IConnection and have the JsonObjectAttribute
             return typeof(IConnection).IsAssignableFrom(objectType) && Attribute.GetCustomAttribute(objectType, typeof(JsonObjectAttribute)) != null;
         }
 
@@ -17,18 +18,24 @@ namespace MILAV.API.Connection
             var token = JToken.ReadFrom(reader);
             if (token is JObject jObject)
             {
+                // Get the first type that has the JsonObjectAttribute with the `Id` property equal to the same value as the "type" property od the JSON object 
                 var ttype = AppDomain.CurrentDomain.GetAssemblies().SelectMany(assembly => assembly.GetTypes())
                     .First(type => ((JsonObjectAttribute)Attribute.GetCustomAttribute(type, typeof(JsonObjectAttribute)))?.Id == (string)jObject["type"]);
+
+                // Use the default deserializer for the specific type found
                 return JsonConvert.DeserializeObject(token.ToString(Formatting.None), ttype);
             }
 
+            // Fail when the token is not an object
             return null;
         }
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
+            // Get the basic JSON object from the value
             var token = JObject.FromObject(value);
             var attribute = (JsonObjectAttribute)Attribute.GetCustomAttribute(value.GetType(), typeof(JsonObjectAttribute));
+            // Add the "type" property with the `Id` from the JsonObjectAttribute which will be used for deserialization
             token["type"] = attribute.Id;
             token.WriteTo(writer);
         }
--- Device/IDeviceConverter.cs
+++ Device/IDeviceConverter.cs
@@ -8,30 +8,38 @@ namespace MILAV.API.Device
     {
         public override bool CanConvert(Type objectType)
         {
+            // True for types that are castable to IDevice and have the DeviceAttribute
             return typeof(IDevice).IsAssignableFrom(objectType) && Attribute.GetCustomAttribute(objectType, typeof(DeviceAttribute)) != null;
         }
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
-            DeviceRegistry.Initialize();
-
             var token = JToken.ReadFrom(reader);
             if (token is JObject jObject)
             {
+                // Ensure that the DeviceRegistry has been initialized before accessing it
+                DeviceRegistry.Initialize();
+
+                // Get the type device type that matches the "type" and "id" properties of the JSON object
                 if (DeviceRegistry.TryGet((DeviceType)Enum.Parse(typeof(DeviceType), (string)jObject["type"], true), (string)jObject["id"], out Type type))
                 {
+                    // Use the default deserializer for the specific type found
                     return JsonConvert.DeserializeObject(token.ToString(Formatting.None), type);
                 }
             }
 
+            // Fail when the token is not an object
             return null;
         }
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
+            // Get the basic JSON object from the value
             var token = JObject.FromObject(value);
             var attribute = (DeviceAttribute)Attribute.GetCustomAttribute(value.GetType(), typeof(DeviceAttribute));
+            // Add the "type" property with the `type` from the DeviceAttribute which will be used for deserialization
             token["type"] = attribute.type.ToString();
+            // Add the "id" property with the `id` from the DeviceAttribute which will be used for deserialization
             token["id"] = attribute.id;
             token.WriteTo(writer);
         }
