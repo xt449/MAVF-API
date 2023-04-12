af09a9583575d931f433333a59838b46277fb17f
Jonathan Talcott <jetalcott@milcorp.com>
2023-04-12 13:24:31 -0400

    Added IRemoteControl interface

---
+++ Device/TVTuner/IRemoteControl.cs
@@ -0,0 +1,41 @@
+﻿namespace MILAV.API.Device.TVTuner
+{
+    public interface IRemoteControl : IPowerControl
+    {
+        public virtual void PowerOn()
+        {
+            SetPower(true);
+        }
+
+        public virtual void PowerOff()
+        {
+            SetPower(false);
+        }
+
+        void ArrowUp();
+
+        void ArrowDown();
+
+        void ArrowLeft();
+
+        void ArrowRight();
+
+        void Enter();
+
+        void Exit();
+
+        void Back();
+
+        void Guide();
+
+        void Menu();
+
+        void ChannelUp();
+
+        void ChannelDown();
+
+        void VolumeUp();
+
+        void VolumeDown();
+    }
+}
