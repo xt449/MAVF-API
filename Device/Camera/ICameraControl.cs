bfd67bb5479bcb3e880b3ff09889c93fd1de2673
Jonathan Talcott <jetalcott@milcorp.com>
2023-04-12 14:12:08 -0400

    Added ICameraControl interface

---
+++ Device/Camera/ICameraControl.cs
@@ -0,0 +1,21 @@
+﻿namespace MILAV.API.Device.Camera
+{
+    public interface ICameraControl
+    {
+        void PanUp();
+
+        void PanDown();
+
+        void PanLeft();
+
+        void PanRight();
+
+        void ZoomIn();
+
+        void ZoomOut();
+
+        void OpenLens();
+
+        void CloseLens();
+    }
+}
