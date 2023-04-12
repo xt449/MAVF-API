5a1011f90be4ff1efdd7fe39ca4811e5173682e5
Jonathan Talcott <jetalcott@milcorp.com>
2023-04-12 14:08:53 -0400

    Added ICodecControl interface

---
+++ Device/Codec/ICodecControl.cs
@@ -0,0 +1,17 @@
+﻿namespace MILAV.API.Device.Codec
+{
+    public interface ICodecControl
+    {
+        public void AddParticipant(string phonebookId);
+
+        public void RemoveParticipant(string phonebookId);
+
+        public void BeginConferenceCall();
+
+        public void EndConferenceCall();
+
+        public void VolumeUp();
+
+        public void VolumeDown();
+    }
+}
