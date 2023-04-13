8e76d5fec18fd32610db90d3dfdb52c7c005f295
Jonathan Talcott <jetalcott@milcorp.com>
2023-04-13 11:30:43 -0400

    Finished basic implementation of WebSocketConnection

--- Connection/IPConnection.cs
+++ Connection/IPConnection.cs
@@ -15,14 +15,15 @@ namespace MILAV.API.Connection
 
         public abstract byte[] ReadBytes(int maxLength = 4096);
 
-        public string ReadASCII(int maxLength = 4096)
+        public virtual string ReadASCII(int maxLength = 4096)
         {
             var data = ReadBytes(maxLength);
             return Encoding.ASCII.GetString(data, 0, data.Length);
         }
 
         public abstract void WriteBytes(byte[] buffer, int offset, int length);
-        public void WriteASCII(string text)
+
+        public virtual void WriteASCII(string text)
         {
             var data = Encoding.ASCII.GetBytes(text);
             WriteBytes(data, 0, data.Length);
--- Connection/WebSocketConnection.cs
+++ Connection/WebSocketConnection.cs
@@ -1,34 +1,57 @@
-﻿namespace MILAV.API.Connection
+﻿using System.Net.WebSockets;
+using System.Text;
+
+namespace MILAV.API.Connection
 {
     internal class WebSocketConnection : IPConnection
     {
+        private readonly ClientWebSocket websocket;
+
         public WebSocketConnection(string ip, int port) : base(ip, port)
         {
+            websocket = new ClientWebSocket();
         }
 
         public override bool Connect()
         {
-            throw new NotImplementedException();
+            using var source = new CancellationTokenSource(5_000);
+            websocket.ConnectAsync(new Uri($"ws://{ip}:{port}"), source.Token).Wait();
+            return !source.IsCancellationRequested;
         }
 
         public override void Disconnect()
         {
-            throw new NotImplementedException();
+            websocket.CloseAsync(WebSocketCloseStatus.NormalClosure, null, CancellationToken.None).Wait();
         }
 
         public override void Dispose()
         {
-            throw new NotImplementedException();
+            websocket.Dispose();
         }
 
         public override byte[] ReadBytes(int maxLength = 4096)
         {
-            throw new NotImplementedException();
+            var buffer = new ArraySegment<byte>(new byte[maxLength]);
+            using var source = new CancellationTokenSource(5_000);
+            var result = websocket.ReceiveAsync(buffer, source.Token).Result;
+            if(result == null)
+            {
+                return new byte[0];
+            }
+            // This should never be null
+            return buffer.Array;
         }
 
         public override void WriteBytes(byte[] buffer, int offset, int length)
         {
-            throw new NotImplementedException();
+            using var source = new CancellationTokenSource(5_000);
+            websocket.SendAsync(new ArraySegment<byte>(buffer, offset, length), WebSocketMessageType.Binary, true, source.Token).Wait();
+        }
+
+        public override void WriteASCII(string text)
+        {
+            using var source = new CancellationTokenSource(5_000);
+            websocket.SendAsync(new ArraySegment<byte>(Encoding.ASCII.GetBytes(text)), WebSocketMessageType.Text, true, source.Token).Wait();
         }
     }
 }
