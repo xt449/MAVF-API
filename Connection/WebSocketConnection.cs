85ec81e8f4bcc02dda63c6b67e0a6f65e88c3926
Jonathan Talcott <jetalcott@milcorp.com>
2023-04-13 11:40:51 -0400

    Renamed client field in WebSocketConnection to match other IPConnection implementations

--- Connection/WebSocketConnection.cs
+++ Connection/WebSocketConnection.cs
@@ -5,35 +5,35 @@ namespace MILAV.API.Connection
 {
     internal class WebSocketConnection : IPConnection
     {
-        private readonly ClientWebSocket websocket;
+        private readonly ClientWebSocket client;
 
         public WebSocketConnection(string ip, int port) : base(ip, port)
         {
-            websocket = new ClientWebSocket();
+            client = new ClientWebSocket();
         }
 
         public override bool Connect()
         {
             using var source = new CancellationTokenSource(5_000);
-            websocket.ConnectAsync(new Uri($"ws://{ip}:{port}"), source.Token).Wait();
+            client.ConnectAsync(new Uri($"ws://{ip}:{port}"), source.Token).Wait();
             return !source.IsCancellationRequested;
         }
 
         public override void Disconnect()
         {
-            websocket.CloseAsync(WebSocketCloseStatus.NormalClosure, null, CancellationToken.None).Wait();
+            client.CloseAsync(WebSocketCloseStatus.NormalClosure, null, CancellationToken.None).Wait();
         }
 
         public override void Dispose()
         {
-            websocket.Dispose();
+            client.Dispose();
         }
 
         public override byte[] ReadBytes(int maxLength = 4096)
         {
             var buffer = new ArraySegment<byte>(new byte[maxLength]);
             using var source = new CancellationTokenSource(5_000);
-            var result = websocket.ReceiveAsync(buffer, source.Token).Result;
+            var result = client.ReceiveAsync(buffer, source.Token).Result;
             if(result == null)
             {
                 return new byte[0];
@@ -45,13 +45,13 @@ namespace MILAV.API.Connection
         public override void WriteBytes(byte[] buffer, int offset, int length)
         {
             using var source = new CancellationTokenSource(5_000);
-            websocket.SendAsync(new ArraySegment<byte>(buffer, offset, length), WebSocketMessageType.Binary, true, source.Token).Wait();
+            client.SendAsync(new ArraySegment<byte>(buffer, offset, length), WebSocketMessageType.Binary, true, source.Token).Wait();
         }
 
         public override void WriteASCII(string text)
         {
             using var source = new CancellationTokenSource(5_000);
-            websocket.SendAsync(new ArraySegment<byte>(Encoding.ASCII.GetBytes(text)), WebSocketMessageType.Text, true, source.Token).Wait();
+            client.SendAsync(new ArraySegment<byte>(Encoding.ASCII.GetBytes(text)), WebSocketMessageType.Text, true, source.Token).Wait();
         }
     }
 }
