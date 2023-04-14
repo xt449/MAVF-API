bafd2a154646f64fce9c7cf4efb4eae34e1cb0ca
Jonathan Talcott <jetalcott@milcorp.com>
2023-04-14 08:31:19 -0400

    Added example CustomVideoSwitcherController

---
+++ Device/Video/VideoSwitcher/CustomVideoSwitcherController.cs
@@ -0,0 +1,35 @@
+﻿using MILAV.API.Device.Video.VideoSwitcher;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+
+namespace MILAV.API.Device.USB
+{
+    public class CustomVideoSwitcherController : AbstractDevice, IVideoSwitcherControl
+    {
+        [JsonProperty(Required = Required.DisallowNull)]
+        public readonly string requestSetRoute;
+
+        [JsonProperty(Required = Required.DisallowNull)]
+        public readonly string responseSetRoute;
+
+        public bool Route(Input input, Output output)
+        {
+            if (input.device != output.device)
+            {
+                return false;
+            }
+
+            if (Connection.Connect())
+            {
+                // RegEx formatting ($1)?
+                // C# formatting ({0})?
+                // or something else?
+                Connection.WriteASCII(requestSetRoute.Replace("$1", input.input.ToString()).Replace("$2", output.output.ToString()));
+
+                return Regex.Match(Connection.ReadASCII(), responseSetRoute).Success;
+            }
+
+            return false;
+        }
+    }
+}
