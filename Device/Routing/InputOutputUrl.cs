3a5709fcb0277d721b20fa8771c711c594609935
Jonathan Talcott <jetalcott@milcorp.com>
2023-04-17 14:23:24 -0400

    Added more basic implementations of IInputOutput for future devices

---
+++ Device/Routing/InputOutputUrl.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace MILAV.API.Device.Routing
+{
+    public class InputOutputUrl : IInputOutput
+    {
+        public string Id { get; init; }
+
+        public IOType Type { get; init; }
+
+        public string Group { get; init; }
+
+        [JsonProperty(Required = Required.DisallowNull)]
+        public readonly string url;
+    }
+}
