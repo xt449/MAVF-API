13edd81d4efadc3d785563c10bd16be1febabfe2
Jonathan Talcott <jetalcott@milcorp.com>
2023-04-24 08:23:12 -0400

    Possibility to reduce IPowerControl interface requirements

--- Device/IPowerControl.cs
+++ Device/IPowerControl.cs
@@ -4,6 +4,7 @@
     {
         public bool GetPower();
 
+        // TODO - Is this ever needed, or should a local variable track the last power state
         public void SetPower(bool state);
     }
 }
