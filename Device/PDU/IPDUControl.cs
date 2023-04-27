5b215085a9a05bf1a02b83ef4b5ce17768682009
Jonathan Talcott <jetalcott@milcorp.com>
2023-04-27 08:41:02 -0400

    Let API get PDU port by connected device

--- Device/PDU/IPDUControl.cs
+++ Device/PDU/IPDUControl.cs
@@ -1,4 +1,7 @@
-﻿namespace MILAV.API.Device.PDU
+﻿using MILAV.API.Device.Routing;
+using Newtonsoft.Json;
+
+namespace MILAV.API.Device.PDU
 {
     /// <summary>
     /// This interfaces funcationilty may be built into instances of IDevice later
@@ -8,5 +11,12 @@
         public void TurnPowerOn(int port);
 
         public void TurnPowerOff(int port);
+
+        /// <summary>
+        /// Key: Device#Id
+        /// Value: Port
+        /// </summary>
+        [JsonProperty("ports", Required = Required.DisallowNull)]
+        public Dictionary<string, int> Ports { get; init; }
     }
 }
