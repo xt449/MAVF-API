063bb99af82dd0e4bf9b11e0aba8b9ffaf79ee30
Jonathan Talcott <jetalcott@milcorp.com>
2023-04-27 13:29:06 -0400

    Start of basis SSH conection protocol

--- Connection/SSHConnection.cs
+++ Connection/SSHConnection.cs
@@ -1,34 +1,69 @@
-﻿namespace MILAV.API.Connection
+﻿using Renci.SshNet;
+using System;
+using System.Collections.Concurrent;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace MILAV.API.Connection
 {
     internal class SSHConnection : NetworkConnection
     {
-        public SSHConnection(string ip, int port) : base(ip, port)
-        {
-        }
+        private readonly SshClient client;
 
-        public override bool Connect()
-        {
-            throw new NotImplementedException();
-        }
+        private ConcurrentQueue<string> responses = new ConcurrentQueue<string>();
 
-        public override void Disconnect()
+        public SSHConnection(string ip, int port, string username, string password) : base(ip, port)
         {
-            throw new NotImplementedException();
-        }
-
-        public override void Dispose()
-        {
-            throw new NotImplementedException();
+            client = new SshClient(ip, port, username, password);
         }
 
         public override byte[] ReadBytes(int maxLength = 4096)
         {
-            throw new NotImplementedException();
+            if(responses.TryDequeue(out var response))
+            {
+                return Encoding.ASCII.GetBytes(response);
+            }
+
+            throw new Exception("No response gotten back from commands");
+        }
+
+        public override string ReadASCII(int maxLength = 4096)
+        {
+            if (responses.TryDequeue(out var response))
+            {
+                return response;
+            }
+
+            throw new Exception("No response gotten back from commands");
         }
 
         public override void WriteBytes(byte[] buffer, int offset, int length)
         {
-            throw new NotImplementedException();
+            responses.Enqueue(client.RunCommand(Encoding.ASCII.GetString(buffer, offset, length)).Execute());
+        }
+
+        public override void WriteASCII(string text)
+        {
+            responses.Enqueue(client.RunCommand(text).Execute());
+        }
+
+        public override bool Connect()
+        {
+            client.Connect();
+
+            return client.IsConnected;
+        }
+
+        public override void Disconnect()
+        {
+            client.Disconnect();
+        }
+
+        public override void Dispose()
+        {
+            client.Dispose();
+            // Compiler says this is good to have
+            GC.SuppressFinalize(this);
         }
     }
 }
