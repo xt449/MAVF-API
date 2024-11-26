64a90bd96259fbef4a559110e420c9f77145d775
Jonathan Talcott <jetalcott@milcorp.com>
2024-11-26 09:36:54 -0500

    Updated WebSocketConnection

--- Connection/WebSocketConnection.cs
+++ Connection/WebSocketConnection.cs
@@ -6,16 +6,18 @@ namespace MAVF.API.Connection
 	internal class WebSocketConnection : NetworkConnection
 	{
 		private readonly ClientWebSocket client;
+		private readonly Uri uri;
 
-		public WebSocketConnection(string ip, int port) : base(ip, port)
+		public WebSocketConnection(string ip, int port, bool secure, string relativeAddress) : base(ip, port)
 		{
 			client = new ClientWebSocket();
+			uri = new Uri($"{(secure ? "wss" : "ws")}://{ip}:{port}/{relativeAddress}");
 		}
 
 		public override bool Connect()
 		{
 			using var source = new CancellationTokenSource(5_000);
-			client.ConnectAsync(new Uri($"ws://{ip}:{port}"), source.Token).Wait();
+			client.ConnectAsync(uri, source.Token).Wait();
 			return !source.IsCancellationRequested;
 		}
 
@@ -34,13 +36,9 @@ namespace MAVF.API.Connection
 			var buffer = new ArraySegment<byte>(new byte[maxLength]);
 			using var source = new CancellationTokenSource(5_000);
 			var result = client.ReceiveAsync(buffer, source.Token).Result;
-			if (result == null)
-			{
-				return new byte[0];
-			}
-#pragma warning disable CS8603 // This will never be null
-			return buffer.Array;
-#pragma warning restore CS8603 // This will never be null
+
+			return result == null ? Array.Empty<byte>() : buffer.Array!;
+			// buffer.Array is never null
 		}
 
 		public override void WriteBytes(byte[] buffer, int offset, int length)
--- Device/AbstractNetworkDevice.cs
+++ Device/AbstractNetworkDevice.cs
@@ -44,10 +44,10 @@ namespace MAVF.API.Device
 						Connection = new HttpConnection(ip, port, false, httpRelativeAddress ?? throw new Exception("Missing httpRelativeAddress for HTTP connection"));
 						break;
 					case Protocol.HTTPS:
-						Connection = new HttpConnection(ip, port, true, httpRelativeAddress ?? throw new Exception("Missing httpRelativeAddress for HTTP connection"));
+						Connection = new HttpConnection(ip, port, true, httpRelativeAddress ?? throw new Exception("Missing httpRelativeAddress for HTTPS connection"));
 						break;
 					case Protocol.WEBSOCKET:
-						Connection = new WebSocketConnection(ip, port);
+						Connection = new WebSocketConnection(ip, port, false, httpRelativeAddress ?? "");
 						break;
 					case Protocol.SSH:
 						Connection = new SSHConnection(ip, port, username ?? throw new Exception("Missing username for SSH connection"), password ?? throw new Exception("Missing password for SSH connection"));
