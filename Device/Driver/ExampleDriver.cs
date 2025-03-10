621a8510f1107360c33dc18f5ed35e65c29876a4
Binary Banana <alpha.xt449.omega@gmail.com>
2025-03-09 21:44:15 -0400

    Removed TestDriver in favor of NoneDriver and ExampleDriver

---
+++ Device/Driver/ExampleDriver.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MAVF.API.Device.Driver
+{
+	// Register IDriver implementing type to DriverRegistry
+	[Driver("test")]
+	public sealed class ExampleDriver : AbstractCommunicationDriver<ExampleDriver.DriverProperties>
+	{
+		// Map constructor for JsonSerializer
+		[JsonConstructor]
+		public ExampleDriver(DriverProperties properties) : base(properties)
+		{
+			Connection.Connect();
+
+			Connection.WriteASCII(Properties.Example);
+		}
+
+		// Records
+
+		public record DriverProperties : CommunicationDriverProperties
+		{
+			// Map property name for JsonSerializer
+			[JsonPropertyName("example")]
+			public required string Example { get; init; }
+		}
+
+		public record DriverProperties2
+		{
+
+		}
+	}
+}
---
+++ Device/Driver/NoneDriver.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MAVF.API.Device.Driver
+{
+	// Register IDriver implementing type to DriverRegistry
+	[Driver("none")]
+	public sealed class NoneDriver : IDriver
+	{
+		public object? Properties => null;
+
+		// Map constructor for JsonSerializer
+		[JsonConstructor]
+		public NoneDriver()
+		{
+		}
+	}
+}
